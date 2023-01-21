c7cea3d89c6b74d9dcfcaf6e3156d5ca96fa30bc
Polina <polina@debian-BULLSEYE-live-builder-AMD64>
2023-01-22 00:37:34 +0300

    Initial commit

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+  describe "GET /index" do
+
+    context "user is not logged in" do
+      it "is successful" do
+        get root_path
+        expect(response).to have_http_status(:success)
+        end
+    end
+
+    context "user is logged in" do
+      it "redirects to dashboard_path" do
+        user = create(:user)
+        sign_in user
+        get root_path
+        expect(response).to have_http_status(:redirect)
+
+      end
+
+    end
+  end
+end
